1038ed3884a27ec857e0d124ae23f9868efe9266
Muh Zayyan Al Thaaf Nur <aatzayyan49@gmail.com>
2025-12-23 18:42:16 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="bg-gray-950 text-white">
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Portfolio <span className="text-blue-500">Zayyan</span>
+      </h1>
+    </section>
+  )
+}
+
+export default Hero
